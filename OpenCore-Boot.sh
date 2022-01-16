4d270b3b71080676c32f4cdde38f368487a7acb4
Sergio-Reeves <97850911+Sergio-Reeves@users.noreply.github.com>
2022-01-16 19:37:09 +0000

    "The Recovery Server Could Not Be Contacted"
    I kept getting this error during the initial QEMU High Sierra install - edited the netdev line (copied it from the simpleKVM basic.sh file. If anyone else is getting this error this worked for me ;D

--- OpenCore-Boot.sh
+++ OpenCore-Boot.sh
@@ -53,7 +53,8 @@ args=(
   -drive id=MacHDD,if=none,file="$REPO_PATH/mac_hdd_ng.img",format=qcow2
   -device ide-hd,bus=sata.4,drive=MacHDD
   # -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
-  -netdev user,id=net0 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
+  #-netdev user,id=net0 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
+  -netdev user,id=net0 -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27
   -monitor stdio
   -device VGA,vgamem_mb=128
 )
